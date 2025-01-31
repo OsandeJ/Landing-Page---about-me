b3f9d561a343941de29c251bd45fc70b8d31070a
Osande de Jesus <osande.lc17@gmail.com>
2025-01-31 09:31:15 +0100

    Responsividade, Menu Mobile e Animações Scroll

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function toggleMenu() {
     menuToggle.classList.toggle('active');
     mobileMenu.classList.toggle('active');
 }
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1); // Remove o '#'
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
