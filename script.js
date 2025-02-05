139131b521e1ad436edf2f0b6414c53fc62c0eea
Osande de Jesus <osande.lc17@gmail.com>
2025-02-05 11:33:33 +0100

    finished

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+document.getElementById("contact-form").addEventListener("submit", async function(event) {
+    event.preventDefault();
+  
+    const formData = {
+      nome: this.nome.value,
+      email: this.email.value,
+      telefone: this.telefone.value,
+      mensagem: this.mensagem.value
+    };
+  
+    const response = await fetch("http://localhost:5000/enviar-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData)
+    });
+  
+    const result = await response.json();
+    if (result.success) {
+      // Mostrar o modal com fade-in
+      const modal = document.getElementById("successModal");
+      modal.classList.add("show");
+      
+      // Resetar o formulário após o sucesso
+      document.getElementById("contact-form").reset();
+  
+      // Esconder o modal com fade-out após 7 segundos
+      setTimeout(function() {
+        modal.classList.remove("show");
+      }, 5000); // 5000 ms = 7 segundos
+    } else {
+      alert(result.error);
+    }
+  });
+  
